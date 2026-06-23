9a99d40bd40e12e5583270fd0d0667746f5d4d45
itsanujworld <147061364+itsanujworld@users.noreply.github.com>
2026-06-23 01:17:34 +0000

    conversions

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,28 @@
+let score = 900
+
+let convertnumber = Number(score)
+
+//console.log(typeof convertnumber)
+//console.log(convertnumber)
+
+let scorebol = 1
+
+//console.log(typeof scorebol)
+
+let scoreconvert = Boolean(scorebol)
+
+//console.log(scoreconvert)
+
+let scoredb = -50
+let scorerd = +scoredb
+
+
+
+
+//console.log(score)
+
+let marks = 20
+//console.log(++marks)
+let result = ++score
+
+console.log(result)
